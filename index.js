15295adfbdd7d6c05958b25a3a0a026993ecb96e
Ethan-Hinds <Ethanator@earthlink.net>
2021-02-21 15:48:21 -0500

    Added index.html

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+app.use(express.json({limit: "1mb"}));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Listening at " + port);
+});
+
 const ChessWebAPI = require('chess-web-api');
 const chessAPI = new ChessWebAPI();
 
